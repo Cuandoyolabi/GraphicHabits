44264cf1ecc511b9f102661d30f46cb25ff9d424
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-07-24 17:11:01 -0600

    "Se actualiza la primera seccion del main"

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+
+
 //Header y Nav 
 
 
@@ -8,8 +10,9 @@ function crearNuevoHabito(event){
 
     event.preventDefault(); //Evitar el formulario por defecto
 
-    const maximoDeElementos = 5;
+    const maximoDeElementos = 2;
     const graphic__container = document.getElementById('graphic__container');
+    const graphic__container__plus = document.getElementById('graphic__container__plus');
     const form = document.getElementById('habitForm');
 
     //Verifica que solo sean 5 elementos
@@ -43,6 +46,7 @@ function crearNuevoHabito(event){
     form.reset();
 };
 
+
 function mostrarlModal(){
     const modal = document.getElementById('modal');
     modal.style.display = 'block';
@@ -59,10 +63,55 @@ document.getElementById('graphic__container__plus').addEventListener('click', mo
 document.querySelector('.close').addEventListener('click', cerrarModal);
 document.getElementById('habitForm').addEventListener('submit', crearNuevoHabito);
 
+//Creando la ventana de color.
+// Simple example, see optional options for more configuration.
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
+
+pickr.on('change', (color) => {
+    document.getElementById('habitColor').value = color.toHEXA().toString();
+});
 
 
-
-const form = document.getElementById('habitForm');
 
 
 
