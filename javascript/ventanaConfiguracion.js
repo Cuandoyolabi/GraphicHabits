937a646a16857bd44348f0233e43a1f3f22de06c
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-12-16 16:36:08 -0600

    Se elimina la ventana emergente de perfil y se remplaza por la de configuracione exitosamente

--- javascript/ventanaConfiguracion.js
+++ javascript/ventanaConfiguracion.js
@@ -2,14 +2,15 @@ export const perfil__emergente__id = document.getElementById("perfil__emergente_
 
 export function mostrarPerfil(){
 
-    perfil__emergente__id.classList.toggle("visible");
-    perfil__emergente__id.classList.remove("hidden");
+    ventana__configuracion__id.classList.toggle("visible");
+    ventana__configuracion__id.classList.remove("hidden");
 
 }
 
 export function cerrarPerfil(){
 
-    perfil__emergente__id.classList.toggle("hidden");
+    ventana__configuracion__id.classList.toggle("hidden");
+    //perfil__emergente__id.classList.toggle("hidden");
 
 }
 
