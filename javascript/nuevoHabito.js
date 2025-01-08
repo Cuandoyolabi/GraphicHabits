e7a11f2a152fa5f42bae25db9e64bc11087b24b9
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-01-08 10:57:26 -0600

    Se hace guardado de progreso porque a continuacion de creara una nueva rama y se implementara la funcion de 'completar' habito

--- javascript/nuevoHabito.js
+++ javascript/nuevoHabito.js
@@ -156,20 +156,18 @@ export  function guardarHabitos(){
     const graphic__container__id = document.getElementById("graphic__container__id");
 
     const habitos = Array.from(habits__container__list__id.children).map((habitItem, index) => {
-        
         //Texto
         const habitElement = habitItem.querySelector(".nuevo__recuadro__Abajo h2");
         const habitText =  habitElement ? habitElement.textContent.replace("✔", "").trim() : "";
         const graphicHabit = graphic__container__id.children[index];
+        //Color
+        const habitColor = graphicHabit.style.backgroundColor;
 
         //Verificacion de que el habito se implemento en la grafica y el contenedor
         if(!graphicHabit){
             console.error(`Error: No se encontro el elemento grafico para el indice ${index}`);
             return null;
         }
-
-        //Color
-        const habitColor = graphicHabit.style.backgroundColor;
 
         //Implementacion de ID
         let habitId =  uuid.v4();
@@ -369,7 +367,3 @@ export function obtenerIdDesdeUrl(){
     const params = new URLSearchParams(window.location.search);
     return params.get("id");
 }
-
-
-
-
